294da0f236f185061f9cf40582a2ee647f2ce88e
Ashish Chauhan <chauhanashish0017@gmail.com>
2022-07-31 14:07:07 +0530

    basics

---
+++ Datatype.py
@@ -0,0 +1,42 @@
+x = 1920
+y = -2022
+z = 4.5     
+x1 = 56.7e100
+y1 = 'Sam'
+z1 = "bruce wayne 01"
+# None 
+x2 = None
+# List
+y2 = [5, 7,8,9,34,23]
+z2 = [8.9,34,2,3,1,6]
+x3 = [9,4,5,3,'python']
+# Tuples
+y3 = (5, 7,8,9,34,23)
+z3 = (9,4,5,3,'python')
+
+# Sets
+x4 = {5,6,4,8,9,10}
+y4 = {9,4,5,3,'python'}
+
+print(x4, type(x4))
+print(y4, type(y4))
+
+
+
+print(x2,type(x2))
+print(x, type(x))
+print(y, type(y))
+print(z, type(z))
+print(x1, type(x1))
+print(y1, type(y1))
+print(z1, type(z1))
+print(y2, type(y2))
+print(z2, type(z2))
+print(x3, type(x3))
+print(y3, type(y3))
+print(z3, type(z3))
+
+# Dictionaries
+
+car_intfo = {"brand": "Ford", "model":'musting',"year": 1965 }
+print(car_intfo,type(car_intfo))
---
+++ operators.py
@@ -0,0 +1,17 @@
+a = 10
+b = 5
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b) # float division
+print(a//b) # integer division
+print(a%b) # remainder
+print(a**b) # exponent
+
+fruits = ['Apple', 'Banana', 'Orange', 'Pear', 'Grape']
+print('papaya' in fruits)
+print('apple' in fruits)
+print('Apple' in fruits)
+print('Apple' not in fruits)
+
